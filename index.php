fcbc3c2e97db2be3841a0f7776b147d060bcc22c
adnane-ka <dannykadri0@gmail.com>
2024-08-12 19:25:46 +0100

    init

--- index.php
+++ index.php
@@ -9,13 +9,13 @@ $gateway = Omnipay::create('Tap');
 $gateway->setApiToken('sk_test_XKokBfNWv6FIYuTMg5sLPjhJ');
 
 $response = $gateway->purchase([
-    'amount' => 1,
-    'currency' => 'KWD', 
+    'amount' => 100,
+    'currency' => 'USD', 
     'customerName' => 'Test',
     'customerEmail' => 'test@test.com',
-    'sourceId' => 'src_card',
-    'threeDSecure' => false,
-    'returnUrl' => 'http://localhost:8000/complete.php'
+    'sourceId' => 'src_all', // src_card, src_all, src_kw.knet
+    'threeDSecure' => false, // 3D Secure Enrolled
+    'returnUrl' => 'http://localhost:8000/complete.php' 
 ])->send();
 
 if ($response->isRedirect()) {
